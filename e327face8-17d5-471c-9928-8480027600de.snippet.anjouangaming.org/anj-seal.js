f54944bab795c0c9e7f5760415fa2ed65e2e7a9b
SBmats <156283216+SBmats@users.noreply.github.com>
2024-06-29 15:39:28 +0530

    Add files via upload

---
+++ e327face8-17d5-471c-9928-8480027600de.snippet.anjouangaming.org/anj-seal.js
@@ -0,0 +1,102 @@
+"use strict";
+(function (_0x2671ee, _0x20ee6e) {
+  var _0x3d4f2b = _0x1472,
+    _0x2b9030 = _0x2671ee();
+  while (!![]) {
+    try {
+      var _0x3fe787 =
+        parseInt(_0x3d4f2b(0xcc)) / 0x1 +
+        -parseInt(_0x3d4f2b(0xdd)) / 0x2 +
+        (-parseInt(_0x3d4f2b(0xdc)) / 0x3) * (parseInt(_0x3d4f2b(0xd3)) / 0x4) +
+        -parseInt(_0x3d4f2b(0xd9)) / 0x5 +
+        -parseInt(_0x3d4f2b(0xda)) / 0x6 +
+        (-parseInt(_0x3d4f2b(0xd0)) / 0x7) * (parseInt(_0x3d4f2b(0xcf)) / 0x8) +
+        (parseInt(_0x3d4f2b(0xce)) / 0x9) * (parseInt(_0x3d4f2b(0xdf)) / 0xa);
+      if (_0x3fe787 === _0x20ee6e) break;
+      else _0x2b9030["push"](_0x2b9030["shift"]());
+    } catch (_0x246c6a) {
+      _0x2b9030["push"](_0x2b9030["shift"]());
+    }
+  }
+})(_0x1693, 0x83c2d);
+function _0x1693() {
+  var _0x5adc29 = [
+    "https://verification.anjouangaming.org",
+    "376444iAcYfW",
+    "location",
+    "init",
+    "e327fce8-17d5-471c-9928-8480027600de",
+    "src=\x22https://e327fce8-17d5-471c-9928-8480027600de.snippet.anjouangaming.org/sealassets/a58e6bb9885a171ae5f8daed4da74dd6-",
+    "anj-e327fce8-17d5-471c-9928-8480027600de",
+    "1783900kTCyyi",
+    "4900332aUiFNU",
+    "data-anj-image-size",
+    "9SmlrsE",
+    "1175140NcFpkP",
+    "getAttribute",
+    "10imAniE",
+    "getElementById",
+    "anj-seal-container",
+    "data-anj-image-type",
+    "691531KsVpqs",
+    "host",
+    "22414050OTjYzv",
+    "8CYcAOr",
+    "4192181mVEmqw",
+    "innerHTML",
+  ];
+  _0x1693 = function () {
+    return _0x5adc29;
+  };
+  return _0x1693();
+}
+function _0x1472(_0x3bcad2, _0x4d4315) {
+  var _0x16937d = _0x1693();
+  return (
+    (_0x1472 = function (_0x1472ab, _0x31aba0) {
+      _0x1472ab = _0x1472ab - 0xc9;
+      var _0x516d08 = _0x16937d[_0x1472ab];
+      return _0x516d08;
+    }),
+    _0x1472(_0x3bcad2, _0x4d4315)
+  );
+}
+var anj_e327fce8_17d5_471c_9928_8480027600de = {
+    init: function () {
+      var _0x46ad9 = _0x1472,
+        _0x2f3595 =
+          document[_0x46ad9(0xc9)](_0x46ad9(0xd8)) ||
+          document[_0x46ad9(0xc9)](_0x46ad9(0xca)),
+        _0x4b19da = _0x46ad9(0xd6),
+        _0xcfd4bf = _0x2f3595[_0x46ad9(0xde)](_0x46ad9(0xcb)),
+        _0x149ec5 = _0x2f3595[_0x46ad9(0xde)](_0x46ad9(0xdb)),
+        _0x2cad71 = "",
+        _0x49b416 = _0x46ad9(0xd2);
+      (_0x2cad71 +=
+        "<div\x20style=\x22display:\x20block;position:\x20relative;overflow:\x20hidden;max-width:\x20" +
+        _0x149ec5 +
+        "px;min-width:\x2032px;\x20background-image:\x20url(https://e327fce8-17d5-471c-9928-8480027600de.snippet.anjouangaming.org/54f396e0-b046-49b1-9cb3-0c69281d7ea9-beacon.png);\x22>"),
+        _0x49b416 != "" &&
+          (_0x2cad71 +=
+            "<a\x20target=\x22_blank\x22\x20rel=\x22nonoopener\x22\x20href=\x22https://verification.anjouangaming.org/validate?domain=" +
+            document[_0x46ad9(0xd4)][_0x46ad9(0xcd)] +
+            "&seal_id=901974114d33d34feb52e270d1ce1dc71c03153016d913cde498e372bb49103a1834ac8fd41b944c3d4e066782331bec&stamp=a58e6bb9885a171ae5f8daed4da74dd6\x22>"),
+        (_0x2cad71 +=
+          "<img\x20alt=\x22\x22\x20style=\x22width:\x20100%;\x20height:\x20auto\x22"),
+        (_0x2cad71 +=
+          _0x46ad9(0xd7) +
+          document["location"]["host"] +
+          "-901974114d33d34feb52e270d1ce1dc71c03153016d913cde498e372bb49103a1834ac8fd41b944c3d4e066782331bec-c2VhbC5wbmc%3D?status=valid\x22>"),
+        _0x49b416 != "" && (_0x2cad71 += "</a>"),
+        (_0x2cad71 += "</div>"),
+        (_0x2f3595[_0x46ad9(0xd1)] = _0x2cad71);
+    },
+  },
+  ANJ = anj_e327fce8_17d5_471c_9928_8480027600de;
+(function () {
+  var _0x43995f = _0x1472;
+  (document["getElementById"]("anj-e327fce8-17d5-471c-9928-8480027600de") !=
+    null ||
+    document[_0x43995f(0xc9)]("anj-seal-container") != null) &&
+    anj_e327fce8_17d5_471c_9928_8480027600de[_0x43995f(0xd5)]();
+})();
